758cfca01b273f78ff109d8e5654ddc1cdc1b646
Anoop CA <anoopafter@gmail.com>
2025-05-23 18:50:41 +0530

    Renamed files

--- correlation_and_regression.py
+++ correlation_and_regression.py
@@ -1,24 +1,19 @@
-# Data
 x = [15, 12, 8, 8, 7, 7, 7, 6, 5, 3]  # Physics
 y = [10, 25,17,11,13,17,20,13, 9,15]  # History
 
-# Number of data points
 n = len(x)
 
-# Means
 mean_x = sum(x) / n
 mean_y = sum(y) / n
 
-# Calculate covariance and variance manually
 cov_xy = sum((x[i] - mean_x) * (y[i] - mean_y) for i in range(n))
-var_x = sum((x[i] - mean_x) ** 2 for i in range(n))
+var_x = sum((x[i] - mean_x)**2 for i in range(n))
 
-# Slope and intercept
 b = cov_xy / var_x
-a = mean_y - b * mean_x
 
-# Predict History score when Physics = 10
+a = mean_y - (b * mean_x)
+
 x_pred = 10
-y_pred = a + b * x_pred
+y_pred = a + (b * x_pred)
 
 print(round(y_pred, 1))
