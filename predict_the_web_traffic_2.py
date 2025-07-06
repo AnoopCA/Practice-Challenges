b2dbc4ebb759ef6ef937d89070191f6ff7defe9b
Anoop CA <anoopafter@gmail.com>
2025-07-06 16:49:13 +0530

    Updated code for web traffic prediction

--- predict_the_web_traffic_2.py
+++ predict_the_web_traffic_2.py
@@ -11,19 +11,13 @@ if __name__ == "__main__":
         data = f.readlines()
     n = int(data[0])
     data = [int(i.strip()) for i in data[1:]]
-
-    sma_list = []
-    wma_list = []
-    ewma_list = []
-    for i in range(1, 31): #len(data)):
-        w = 90
-        w_len = min(i, w)
-        window_data = pd.Series(data[n-w:n-w+i+1])
-        print(len(window_data))
-        weights = np.arange(1, min(i+1,w+1))
-        wma = window_data.iloc[-w_len:] * weights
+    
+    w = 500
+    weights = np.arange(1, w+1)
+    for i in range(1, 31):
+        window_data = pd.Series(data)
+        wma = window_data.iloc[-w:] * weights
         wma = round(int(wma.sum() / weights.sum()),1)
-        #wma_list.append(wma)
         data.append(wma)
         data.pop(0)
-        #print(data[-1])
+        print(wma)
