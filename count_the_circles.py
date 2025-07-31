2527fae3ddc6c0d62d854e08a83fba78e374e807
Anoop CA <anoopafter@gmail.com>
2025-08-01 00:22:25 +0530

    Updated code for couting circles

--- count_the_circles.py
+++ count_the_circles.py
@@ -1,8 +1,31 @@
 import sys
 import numpy as np
 
-data = sys.stdin.read().strip().split('\n')
+#data = sys.stdin.read().strip().split('\n')
+with open('count_the_circles_test_cases.txt', 'r') as f:
+    data = f.readlines()
 
-pixels = [[[int(k) for k in j.split(',')] for j in i.split()] for i in data[1:]]
+pixels = [[int(sum(int(k) for k in j.split(','))/3) for j in i.split()] for i in data[1:]]
 
-print(pixels)
+idx_lst = []
+
+for i in pixels:
+    temp_list = []
+    for j in i:
+        if j < 200:
+            temp_list.append(1)
+        else:
+            temp_list.append(0)
+    idx_lst.append(temp_list)
+
+#with open('text_out.txt', 'w') as f:
+#    for l in idx_lst:
+#        line = ''.join(str(i) for i in l)
+#        f.write(line + '\n')
+
+for idx_1,row in enumerate(idx_lst):
+    temp_lst = []
+    for idx_2,elem in enumerate(row):
+        if elem == 1:
+            temp_lst.append(idx_2)
+    print(len(temp_lst))
